3fde5afcd0d958dfd994cf7ced4b8ae03bae4b5f
utrut <34451578+Uhtred009@users.noreply.github.com>
2024-12-12 22:18:04 +0800

    Create installclient.sh

---
+++ installclient.sh
@@ -0,0 +1,48 @@
+set -ex
+
+WORKSPACE=/opt/ServerStatus
+mkdir -p ${WORKSPACE}
+cd ${WORKSPACE}
+
+# 下载, arm 机器替换 x86_64 为 aarch64
+OS_ARCH="x86_64"
+latest_version=$(curl -m 10 -sL "https://api.github.com/repos/zdz/ServerStatus-Rust/releases/latest" | grep "tag_name" | head -n 1 | awk -F ":" '{print $2}' | sed 's/\"//g;s/,//g;s/ //g')
+
+wget --no-check-certificate -qO "client-${OS_ARCH}-unknown-linux-musl.zip"  "https://github.com/zdz/ServerStatus-Rust/releases/download/${latest_version}/client-${OS_ARCH}-unknown-linux-musl.zip"
+
+unzip -o "client-${OS_ARCH}-unknown-linux-musl.zip"
+
+# 提示用户输入用户名
+read -p "请输入用户名(-u 参数): " USERNAME
+
+# 修改 stat_client.service 文件
+SERVICE_FILE=/etc/systemd/system/stat_client.service
+
+cat > $SERVICE_FILE <<EOL
+[Unit]
+Description=ServerStatus-Rust Client
+After=network.target
+
+[Service]
+User=root
+Group=root
+Environment="RUST_BACKTRACE=1"
+WorkingDirectory=/opt/ServerStatus
+ExecStart=/opt/ServerStatus/stat_client -a "http://8.210.162.25:8080/report" -u $USERNAME -p mm.erzi
+ExecReload=/bin/kill -HUP \$MAINPID
+Restart=on-failure
+
+[Install]
+WantedBy=multi-user.target
+EOL
+
+# 设置服务权限并启用自启动
+chmod 644 $SERVICE_FILE
+systemctl daemon-reload
+systemctl enable stat_client
+
+# 启动服务
+systemctl start stat_client
+
+# 检查服务状态
+systemctl status stat_client
