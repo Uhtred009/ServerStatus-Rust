0105dbee21f5b87fc421920487d1a5ddf9af48f3
utrut <34451578+Uhtred009@users.noreply.github.com>
2024-12-12 22:29:40 +0800

    Update installclient.sh

--- installclient.sh
+++ installclient.sh
@@ -8,7 +8,8 @@ cd ${WORKSPACE}
 OS_ARCH="x86_64"
 latest_version=$(curl -m 10 -sL "https://api.github.com/repos/zdz/ServerStatus-Rust/releases/latest" | grep "tag_name" | head -n 1 | awk -F ":" '{print $2}' | sed 's/\"//g;s/,//g;s/ //g')
 
-wget --no-check-certificate -qO "client-${OS_ARCH}-unknown-linux-musl.zip"  "https://github.com/zdz/ServerStatus-Rust/releases/download/${latest_version}/client-${OS_ARCH}-unknown-linux-musl.zip"
+#wget --no-check-certificate -qO "client-${OS_ARCH}-unknown-linux-musl.zip"  "https://github.com/zdz/ServerStatus-Rust/releases/download/${latest_version}/client-${OS_ARCH}-unknown-linux-musl.zip"
+wget --no-check-certificate -O "client-${OS_ARCH}-unknown-linux-musl.zip" "https://github.com/zdz/ServerStatus-Rust/releases/download/${latest_version}/client-${OS_ARCH}-unknown-linux-musl.zip"
 
 unzip -o "client-${OS_ARCH}-unknown-linux-musl.zip"
 
